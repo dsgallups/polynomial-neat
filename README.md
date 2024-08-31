412238ba4993c9a4941fdf92af929f1c23869364
dsgallups <dsgallups@protonmail.com>
2024-08-31 08:00:49 -0400

    chore: modules, roadmap init

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Based on the original paper
 
 K. O. Stanley and R. Miikkulainen, "Evolving Neural Networks through Augmenting Topologies," in Evolutionary Computation, vol. 10, no. 2, pp. 99-127, June 2002, doi: 10.1162/106365602320169811.
+
+### Feature Roadmap:
+
+- [ ] Core implementation
+  - [ ] Candle
+  - [ ] nalgebra
+  - [ ] topology
+- [ ] Feed Forward Net
---
+++ src/bin/candle_sandbox/main.rs
@@ -0,0 +1,39 @@
+use candle_core::{Device, Result, Tensor};
+
+struct Model {
+    first: Tensor,
+    second: Tensor,
+}
+
+impl Model {
+    fn example(device: &Device) -> Result<Self> {
+        let first = Tensor::randn(0f32, 1.0, (784, 100), device)?;
+        let second = Tensor::randn(0f32, 1.0, (100, 10), device)?;
+        Ok(Self { first, second })
+    }
+    fn forward(&self, image: &Tensor) -> Result<Tensor> {
+        let x = image.matmul(&self.first)?;
+        let x = x.relu()?;
+        x.matmul(&self.second)
+    }
+}
+
+fn main() -> Result<()> {
+    let device = Device::new_cuda(0)?;
+    let model = Model::example(&device)?;
+
+    let dummy_image = Tensor::randn(0f32, 1.0, (1, 784), &device)?;
+
+    let digit = model.forward(&dummy_image)?;
+
+    println!("digit: {:?}", digit);
+
+    Ok(())
+}
+
+#[test]
+fn test_dev() {
+    let device = Device::cuda_if_available(ordinal).unwrap();
+
+    println!("Device: {:?}");
+}
